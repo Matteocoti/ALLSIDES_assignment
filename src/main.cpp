95411c667d1c9b015e18646a464c24777d71851e
matteo.cotifava <matteo.cotifava@sadel.it>
2026-05-25 22:28:24 +0200

    main: added frame generation using FakeCamera class

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include "SyntheticScene.hpp"
+#include "FakeCamera.hpp"
 
 int main(int argc, char *argv[])
 {
+    SyntheticScene<4504, 4504> scene;
+
+    FakeCamera<4504, 4504> fake_camera(scene);
+
+    BaseFrame fshort = fake_camera.grab(ExposureTime::Short);
+    BaseFrame fmedium = fake_camera.grab(ExposureTime::Medium);
+    BaseFrame flong = fake_camera.grab(ExposureTime::Long);
+
     return 0;
 }
