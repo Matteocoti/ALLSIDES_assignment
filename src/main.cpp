e85df9ae0c53efb3b566530bb9485d86bef786e1
matteo.cotifava <matteo.cotifava@sadel.it>
2026-05-26 19:00:47 +0200

    main: used std::chrono to define a small benchmarking for grab method

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
+#include <iostream>
 #include "SyntheticScene.hpp"
 #include "FakeCamera.hpp"
+
+auto timeDiff(std::chrono::steady_clock::time_point a, std::chrono::steady_clock::time_point b)
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(b - a).count();
+}
 
 int main(int argc, char *argv[])
 {
     SyntheticScene<4504, 4504> scene;
     FakeCamera<4504, 4504> camera(scene);
 
+    auto t0 = std::chrono::steady_clock::now();
     CameraFrame<4504, 4504> fshort = camera.grab(ExposureTime::Short);
+    auto t1 = std::chrono::steady_clock::now();
     CameraFrame<4504, 4504> fmedium = camera.grab(ExposureTime::Medium);
+    auto t2 = std::chrono::steady_clock::now();
     CameraFrame<4504, 4504> flong = camera.grab(ExposureTime::Long);
+    auto t3 = std::chrono::steady_clock::now();
 
+    std::cout << "Short:  " << timeDiff(t0, t1) << " ms\n";
+    std::cout << "Medium: " << timeDiff(t1, t2) << " ms\n";
+    std::cout << "Long:   " << timeDiff(t2, t3) << " ms\n";
 
     fshort.toPGM("short.pgm", 4095);
     fmedium.toPGM("medium.pgm", 4095);
