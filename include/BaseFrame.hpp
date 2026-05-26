7f8a9fa18fcff12730e432a261aaaa549f8de29d
matteo.cotifava <matteo.cotifava@sadel.it>
2026-05-26 18:27:08 +0200

    Added method to convert baseFrame data to PGM

--- include/BaseFrame.hpp
+++ include/BaseFrame.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
+#include <string>
 #include <vector>
 
 template <typename type, int W, int H>
@@ -34,4 +38,27 @@ class BaseFrame
     {
         return data_[i];
     }
+
+    bool toPGM(const std::string &filePath, const type max_value) const;
 };
+
+template <typename type, int W, int H>
+bool BaseFrame<type, W, H>::toPGM(const std::string &filePath, const type max_value) const
+{
+    std::ofstream f(filePath, std::ios::binary);
+
+    f << "P5" << std::endl;
+    f << WIDTH << " " << HEIGHT << std::endl;
+    f << 65535 << std::endl;
+
+    for(const auto &pxl : data_) {
+        float norm = static_cast<float>(std::clamp(pxl, static_cast<type>(0), max_value)) / static_cast<float>(max_value);
+
+        uint16_t val = static_cast<uint16_t>(norm * 65535.0f);
+
+        f.put(val >> 8);
+        f.put(val & 0xFF);
+    }
+
+    return true;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,14 @@ int main(int argc, char *argv[])
     FakeCamera<4504, 4504> fake_camera(scene);
 
     BaseFrame fshort = fake_camera.grab(ExposureTime::Short);
+
+    fshort.toPGM("short.pgm", 4096);
+
     BaseFrame fmedium = fake_camera.grab(ExposureTime::Medium);
+
+    fmedium.toPGM("medium.pgm", 4096);
     BaseFrame flong = fake_camera.grab(ExposureTime::Long);
 
+    flong.toPGM("long.pgm", 4096);
     return 0;
 }
